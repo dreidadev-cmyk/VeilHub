dc54288770407b3b52b8afe2e29fb6cd126022dc
dreidadev-cmyk <dreidadev@gmail.com>
2026-09-16 23:57:11 +0800

    Update Build a Cow Empire entry in manifest

--- games/manifest.lua
+++ games/manifest.lua
@@ -3,6 +3,6 @@ return {
     [2753915549] = { name = "Blox Fruits · Sea 1", file = "blox_fruits.lua" },
     [4442272183] = { name = "Blox Fruits · Sea 2", file = "blox_fruits.lua" },
     [7449423635] = { name = "Blox Fruits · Sea 3", file = "blox_fruits.lua" },
-    -- Build a Cow Empire
-    [PLACEID_HERE] = { name = "Build a Cow Empire", file = "build_a_cow_empire.lua" },
+    -- Build a Cow Empire / Milk Tycoon
+    [10456811278] = { name = "Build a Cow Empire", file = "build_a_cow_empire.lua" },
 }
