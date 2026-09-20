3269a5908c8350be656f714842b4322654075c08
dreidadev-cmyk <dreidadev@gmail.com>
2026-09-20 09:14:19 +0800

    Delete games directory

--- games/manifest.lua
+++
@@ -1,8 +0,0 @@
-return {
-    -- Blox Fruits
-    [2753915549] = { name = "Blox Fruits · Sea 1", file = "blox_fruits.lua" },
-    [4442272183] = { name = "Blox Fruits · Sea 2", file = "blox_fruits.lua" },
-    [7449423635] = { name = "Blox Fruits · Sea 3", file = "blox_fruits.lua" },
-    -- Build a Cow Empire / Milk Tycoon
-    [97269037953752] = { name = "Build a Cow Empire", file = "build_a_cow_empire.lua" },
-}
